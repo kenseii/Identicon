edac05a6b0171406b98e0aee51cb14c095c9e706
oasis <oasisagano@gmail.com>
2019-02-04 16:06:50 +0900

    Added color key,value in the struct

--- lib/image.ex
+++ lib/image.ex
@@ -1,7 +1,7 @@
 # This module holds the code of the image struct
 
 defmodule Identicon.Image do
-  # Define a struct to hold the hashed data
-  defstruct hex: nil
+  # Define a struct to hold the hashed data, and rgb color if there
+  defstruct hex: nil, color: nil
 
 end
