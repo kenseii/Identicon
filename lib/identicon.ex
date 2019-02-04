9ecaa975b07c0b9f1fe3683069ce6e87780939c0
oasis <oasisagano@gmail.com>
2019-02-04 14:13:05 +0900

    Created a struct to hold image hex data

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,25 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+
+  # This function receives an `input string` to be hashed from the user,
+  # After that it pipes it in the hash_input method to change it to MD5
+  def main(input) do
+    input
+    |> hash_input
+
+  end
+
 
   @doc """
-  Hello world.
+    This function hashes the given input using MD5 Algorithm.
+    it takes the given input -> hashed binary.
+    Changes the hashed binary to a list of numbers.
 
-  ## Examples
-
-      iex> Identicon.hello()
-      :world
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
 
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
---
+++ lib/image.ex
@@ -0,0 +1,7 @@
+# This module holds the code of the image struct
+
+defmodule Identicon.Imange do
+  # Define a struct to hold the hashed data
+  defstruct hex: nil
+
+end
