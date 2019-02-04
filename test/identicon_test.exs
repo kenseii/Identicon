31c543f9f84582691a455f645ea3f00924417c5b
oasis <oasisagano@gmail.com>
2019-02-04 15:35:38 +0900

    Added main function test

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -3,6 +3,10 @@ defmodule IdenticonTest do
   doctest Identicon
 
   test "tests the main function calls the hasher" do
-    assert Identicon.main("banana") == Identicon.hash_input("banana")
+    assert Identicon.main("banana") == Identicon.pick_color(
+             %Identicon.Image{
+               hex: [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+             }
+           )
   end
 end
