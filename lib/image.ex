20750b44629b226892ec6e32b6bc77572dda6847
oasis <oasisagano@gmail.com>
2019-02-10 20:04:56 +0900

    Added pixel_map to the struct

--- lib/image.ex
+++ lib/image.ex
@@ -1,7 +1,7 @@
 # This module holds the code of the image struct
 
 defmodule Identicon.Image do
-  # Define a struct to hold the hashed data, and rgb color if there, and hold the grid data once generated
-  defstruct hex: nil, color: nil, grid: nil
+  # Define a struct to hold the hashed data, and rgb color if there, and hold the grid data once generated, hold colored pixel coordinates
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 
 end
