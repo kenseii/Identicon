d5b271810fcbd396615a08d3615b46025349fa68
oasis <oasisagano@gmail.com>
2019-02-10 18:26:51 +0900

    Added grid to image struct

--- lib/image.ex
+++ lib/image.ex
@@ -1,7 +1,7 @@
 # This module holds the code of the image struct
 
 defmodule Identicon.Image do
-  # Define a struct to hold the hashed data, and rgb color if there
-  defstruct hex: nil, color: nil
+  # Define a struct to hold the hashed data, and rgb color if there, and hold the grid data once generated
+  defstruct hex: nil, color: nil, grid: nil
 
 end
