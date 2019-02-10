2415887aac28b8cd1ef453ba2347d7aaf9217e59
oasis <oasisagano@gmail.com>
2019-02-10 18:47:36 +0900

    Updated docs, added the filter_odd tuples method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,13 +1,18 @@
 defmodule Identicon do
 
-  # This function receives an `input string` to be hashed from the user,
-  # After that it pipes it in the `hash_input` method to change it to MD5.
-  # the hashed list is piped in the `pick_color`
+  @doc """
+  This function receives an `input string` to be hashed from the user,
+  after that it pipes it in the `hash_input` method to change it to MD5.
+  The hashed list is piped in the `pick_color` that selects the first 3 elements of the list as RGB and sets it as a color section
+  ,after setting the color section, it calls the build_grid method to create the final image struct.
+  Call the `filter_odd_square` to remove all the odd values in the grid as only the even ones will be colored
+  """
   def main(input) do
     input
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
   end
 
 
@@ -56,22 +61,26 @@ defmodule Identicon do
   After that it calls a `mirror_function` that duplicate the first 2 elements of the chunk to the right.
   `[1,2,3]` becomes `[1,2,3,2,1].
   Enum.map function here calls the `mirror_rows` and pass it every row like a for loop.
-  Lastly it changes the output of mirror_rows from a `list of lists` to a `single list of elements`.
+  After it changes the output of mirror_rows from a `list of lists` to a `single list of elements`.
+  Lastly it appends the new grid value to the image struct
 
 
   """
   def build_grid(%Identicon.Image{hex: hex} = image_struct) do
-    hex
-    |> Enum.chunk(3)
+    grid =
+      hex
       # This also can work, but not that much functional
       # Enum.chunk_every(image_struct.hex,3)
-    |> Enum.map(&mirror_rows/1)
-      # makes it a single list of elements
-    |> List.flatten
-      # add indexes to the flattened list
-    |> Enum.with_index
+      |> Enum.chunk(3)
+        # Calls the mirror rows to duplicate the first 2 elements of every list at the end
+      |> Enum.map(&mirror_rows/1)
+        # makes it a single list of elements
+      |> List.flatten
+        # add indexes to the flattened list
+      |> Enum.with_index
 
-
+    # Add the grid to the image struct
+    %Identicon.Image{image_struct | grid: grid}
 
   end
 
@@ -81,12 +90,39 @@ defmodule Identicon do
   Note: It can do the same by using the Enum module, `.take` to take the first 2 elements,
   and `.reverse` to reverse the first 2 elements then finally append it at the end using ++.
 
+  Note: This is used as a helper function.
+
   """
   def mirror_rows(row) do
     # patter matching to get the 1st and 2nd
     [first, second | _tail] = row
     # add it to the end
     row ++ [second, first]
+
+  end
+
+  @doc """
+  This method removes all odd tuples in the grid so that only the even ones can be colored.
+  It starts by taking each tuple and comparing its first element(using `elem(0)`)'s reminder to a divide by 2.
+  In case the reminder is 0 it is kept other wise not.
+  Then return the new image struct.
+
+  Note: `fn` is a helper function too, like lambdas in python i guess
+  """
+
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image_struct) do
+    # even tuple keeper
+    grid = Enum.filter(
+      grid,
+      fn x ->
+        rem(
+          x
+          |> elem(0),
+          2
+        ) == 0 end
+    )
+    # update the grid to only hold the even numbers
+    %Identicon.Image{image_struct | grid: grid}
 
   end
 
